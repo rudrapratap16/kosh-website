f5bdbe48153cfdb54a6a0352bcc8132136a06d85
rudrapratap16 <rudrapratap16005@gmail.com>
2025-11-10 14:20:12 +0530

    Updated backend

--- backend/app.py
+++ backend/app.py
@@ -41,5 +41,5 @@ def health():
     return "OK", 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("BACKEND_PORT", 8080))
     app.run(host="0.0.0.0", port=port)
